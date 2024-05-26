f704de564e351a7ae13a67f8ef2d09b9d73a8863
tarun7180 <mailtarunjain@gmail.com>
2024-05-26 21:10:34 +0530

    Update Employee.java

--- Employeelist/src/main/java/com/tarun/employeelist/model/Employee.java
+++ Employeelist/src/main/java/com/tarun/employeelist/model/Employee.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
